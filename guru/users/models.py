97233721a0d443c74c50a8172a5d5e8535a3648d
Jerome <jerome.schmidt@students.makeschool.com>
2020-02-16 13:57:24 -0600

    outlined user model fields

--- guru/users/models.py
+++ guru/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, IntegerField, FloatField
+# from django_mysql.models import ListTextField
+from django.contrib.postgres.fields import ArrayField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,6 +11,18 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    # support_skills = ListTextField(base_field=CharField(),size=20)
+    # support_classes_taken = ListTextField(base_field=CharField(),size=20)
+    support_skills = ArrayField(base_field=CharField(), size=20)
+    support_classes_taken = ArrayField(base_field=CharField(), size=20)
+    email_address = contact_info = CharField(max_length=100, default="")
+    is_teachingassistant = (('F', 'False'), ('T', 'True'))
+    rating = IntegerField()
+    avg_reponse = FloatField()
+    is_online = (('F', 'False'), ('T', 'True'))
+    messages_received = IntegerField()
+    Bio = CharField(max_length=500)
+    slug = CharField(max_length=600, blank=True, editable=False, help_text="Unique URL path to access this page.")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
