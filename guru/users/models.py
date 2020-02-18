ddf0ba2fdbdb2c8ab2b18a404343b9815b8be4b4
Jerome <jerome.schmidt@students.makeschool.com>
2020-02-18 09:56:21 -0800

    added features for user

--- guru/users/models.py
+++ guru/users/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-# , IntegerField, FloatField
-# from django_mysql.models import ListTextField
-# from django.contrib.postgres.fields import ArrayField
+from django.db.models import CharField, IntegerField, FloatField
+from django.contrib.postgres.fields import ArrayField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,15 +10,15 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    # support_skills = ArrayField(base_field=CharField(),size=20)
-    # support_classes_taken = ArrayField(base_field=CharField(),size=20)
-    # email_address = contact_info = CharField(max_length=100, default="")
-    # is_teachingassistant = (('F', 'False'), ('T', 'True'))
-    # rating = IntegerField()
-    # avg_reponse = FloatField()
-    # is_online = (('F', 'False'), ('T', 'True'))
-    # messages_received = IntegerField()
-    # Bio = CharField(max_length=500)
+    support_skills = ArrayField(null=True, base_field=CharField(max_length=255), size=20)
+    support_classes_taken = ArrayField(null=True, base_field=CharField(max_length=255), size=20)
+    email_address = CharField(max_length=100, default="")
+    is_teachingassistant = (('F', 'False'), ('T', 'True'))
+    rating = IntegerField(null=True, blank=True)
+    avg_reponse = FloatField(null=True, blank=True)
+    is_online = (('F', 'False'), ('T', 'True'))
+    messages_received = IntegerField(null=True, blank=True)
+    Bio = CharField(blank=True, max_length=500)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
