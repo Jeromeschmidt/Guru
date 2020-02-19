03bfcbdba7d28b13d8806deea97860cdabc473b6
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-19 04:17:16 -0800

    make guru test package with canary

---
+++ guru/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import Client
+
+from guru.views import HomeView
+
+class TestHomeView:
+    def test_canary(self):
+        assert True is True
+
+    def test_get_success_url(self):
+        # c = Client()
+        # response = c.get("")
+        # assert response.status_code == 200
+        # assert 3 == 3
+        pass
