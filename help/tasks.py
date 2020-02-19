cf96dec1b200e492ba496c9ab530b040cc528b37
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-19 05:27:36 -0800

    fix import bug

--- help/tasks.py
+++ help/tasks.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import, unicode_literals
 
 from config.celery_app import app
 from celery import shared_task
-from django.core import mail.send_mail
+from django.core.mail import send_mail as django_send_mail
 from .helpers import *
+
 
 @shared_task
 def send_mail(subject: str, body: str, from_email: str, to_emails: list):
     """Wrapper function for Django's send_mail."""
-    mail.send_mail(
+    django_send_mail(
         subject,
         body,
         from_email,
