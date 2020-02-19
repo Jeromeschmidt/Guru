d9306d82bbd9fbda9df75e22c43adaa5a47b26fb
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-19 06:04:36 -0800

    add title to context

--- guru/views.py
+++ guru/views.py
@@ -2,8 +2,9 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'pages/home.html'
+    template_name = "pages/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["title"] = "Help Guru"
         return context
