4884f25b456b82d55739133b24c9920b370bb5bd
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-19 11:17:57 -0800

    add lang swap function

--- guru/static/js/project.js
+++ guru/static/js/project.js
@@ -1,1 +1,36 @@
 /* Project specific Javascript goes here. */
+
+(function() {
+    "use strict";
+
+    const langDropdownNode = $("#lang-dropdown");
+    const curLangNode = langDropdownNode.find("button");
+    const langOptionsMap = langDropdownItems();
+    const topSubdomain = topSubdomain();
+    const curLanguage = (topSubdomain in langOptionsMap ? topSubdomain : "En");
+
+    (function () {
+        /* -----------------------------------------------
+            Adjust language dropdown based on subdomain
+        ----------------------------------------------- */
+        const origLang = curLangNode.text;
+        curLangNode.text = curLanguage;
+        langOptionsMap[curLanguage] = origLang;
+    })();
+
+    function langDropdownItems() {
+        const langOptions = langDropdownNode.find("div").children();
+        let langNodeMap = {};
+        // iterate over language dropdown options
+        for (let i=0; i<langOptions.length; i++) {
+            const node = langOptions[i];
+            const optionText = node.text.toLowerCase();
+            langNodeMap[optionText] = node;
+        }
+        return langNodeMap;
+    }
+
+    function topSubdomain() {
+        return window.location.host[0].toLowerCase();
+    }
+})();
