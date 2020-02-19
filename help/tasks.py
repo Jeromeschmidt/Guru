ca6db46dd0f467f4526c0943096f684048b10d4d
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-19 02:54:26 -0800

    add email sending task

---
+++ help/tasks.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+
+from config.celery_app import app
+from celery import shared_task
+from django.core import mail.send_mail
+from .helpers import *
+
+@shared_task
+def send_mail(subject: str, body: str, from_email: str, to_emails: list):
+    """Wrapper function for Django's send_mail."""
+    mail.send_mail(
+        subject,
+        body,
+        from_email,
+        to_emails,
+        fail_silently=False,
+    )
