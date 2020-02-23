783ac361e710f7edea8b0a1b9310a7476bcee036
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-23 15:22:14 -0800

    correct template name

--- help/views.py
+++ help/views.py
@@ -3,11 +3,12 @@ from django.views.generic import TemplateView
 
 
 class FindView(TemplateView):
-    template_name = "pages/find.html"
+    template_name = "pages/find_help.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Find Help'
+        context["title"] = "Find Help"
+        context["topics"] = (None, ) * 8
         return context
 
     def post(self, request, *args, **kwargs):
