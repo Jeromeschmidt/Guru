5b13d7b9bf76349698747243fbce67b1f5070ed5
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-24 09:36:34 -0800

    update find help view with login restriction

--- help/views.py
+++ help/views.py
@@ -1,14 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class FindView(TemplateView):
+@login_required
+class FindHelpView(TemplateView):
     template_name = "pages/find_help.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Find Help"
-        context["topics"] = (None, ) * 8
+        context["topics"] = (None,) * 8
         return context
 
     def post(self, request, *args, **kwargs):
