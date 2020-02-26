df6911ea6eef94e12737d0e115043164dd2d4bf0
Jonasz Karwacki <jonaszakr@gmail.com>
2020-02-26 09:59:02 -0800

    add updated user model

--- guru/users/models.py
+++ guru/users/models.py
@@ -12,28 +12,20 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    USER_TYPE_CHOICES = (
-        (1, 'customer'),
-        (2, 'staff'),
-        (3, 'admin'),
-    )
-    user_type = PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    is_customer = BooleanField(_("Customer Status"), default=True)
+    user = OneToOneField(User, on_delete=CASCADE, primary_key=True)
+    skills = ArrayField(_("A list of skills that user can help with"), null=True,
+                        base_field=CharField(max_length=255),
+                        size=20)
+    classes_taken = ArrayField(null=True,
+                               base_field=CharField(max_length=255),
+                               size=20)
+    is_teachingassistant = BooleanField()
+    rating = IntegerField(null=True, blank=True)
+    avg_reponse = FloatField(null=True, blank=True)
+    is_online = BooleanField()
+    messages_received = IntegerField(null=True, blank=True)
+    bio = CharField(blank=True, max_length=500)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
-
-
-class Customer(Model):
-    user = OneToOneField(User, on_delete=CASCADE, primary_key=True)
-    # skills = ArrayField(_("A list of skills that user can help with"), null=True,
-    #                     base_field=CharField(max_length=255),
-    #                     size=20)
-    # classes_taken = ArrayField(null=True,
-    #                            base_field=CharField(max_length=255),
-    #                            size=20)
-    # is_teachingassistant = BooleanField()
-    # rating = IntegerField(null=True, blank=True)
-    # avg_reponse = FloatField(null=True, blank=True)
-    # is_online = BooleanField()
-    # messages_received = IntegerField(null=True, blank=True)
-    bio = CharField(blank=True, max_length=500)
