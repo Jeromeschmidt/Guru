779745c7783647ed6efdaabf784f64c0eabbaa14
Jerome <jerome.schmidt@students.makeschool.com>
2020-02-29 15:11:24 -0800

    sends email message to terminal with only what they need help with, not the description

--- help/tasks.py
+++ help/tasks.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from config.celery_app import app
 from celery import shared_task
 from django.core.mail import send_mail as django_send_mail
-from .helpers import *
+# from .helpers import *
 
 
-@shared_task
+# @shared_task
 def send_mail(subject: str, body: str, from_email: str, to_emails: list):
     """Wrapper function for Django's send_mail."""
     django_send_mail(
--- help/views.py
+++ help/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template import RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 
 from guru.users.models import User
+from django.core.mail import send_mail as django_send_mail
+# from .forms import NameForm
 
 # from config.settings import env
 
@@ -30,13 +32,31 @@ class FindHelpView(TemplateView):
                         skill_list.append(skill)
             except:
                 pass
-        print(skill_list)
         return HttpResponse(render(request, 'pages/find_help.html',
            {'skill_list': skill_list}))
 
 class ConfirmationView(TemplateView):
     template_name = "pages/confirmation.html"
 
+    def get(self, request):
+        # form = NameForm(request.POST)
+        # django_send_mail('Subject here', 'Here is the message', 'from@example.com', ['first@example.com', 'other@example.com'])
+        print(request.GET)
+        subject = request.GET['skill_list-options']
+        message = "I need help with " + subject
+        sender = "jerome.schmidt@students.makeschool.com"
+        # cc_myself = form.cleaned_data['cc_myself']
+
+        recipients = ["jerome.schmidt@students.makeschool.com"]
+        # if cc_myself:
+        #     recipients.append(sender)
+
+        django_send_mail(subject, message, sender, recipients)
+
+        return HttpResponse(render(request, 'pages/confirmation.html',
+           {}))
+
+           
     def post(self, request, *args, **kwargs):
-                return HttpResponse(render(request, 'pages/confirmation.html',
-                   {}))
+        return HttpResponse(render(request, 'pages/confirmation.html',
+           {}))
