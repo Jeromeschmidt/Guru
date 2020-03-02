1291e17a608b5667d2c6ddbf66c3ee0e5625ab6b
Jerome <jerome.schmidt@students.makeschool.com>
2020-03-01 16:46:58 -0800

    minor fix

--- guru/users/admin.py
+++ guru/users/admin.py
@@ -9,7 +9,7 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 
 
-
+                
 # from django.contrib import admin
 # from django.contrib.auth import admin as auth_admin
 # from django.contrib.auth import get_user_model
